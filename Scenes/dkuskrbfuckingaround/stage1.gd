5c5c80cf4759c3d7a01d62e8dcac115a227d63aa
Kenneth Herndon <kengherndon2@gmail.com>
2025-09-04 20:39:51 -0500

    attempting to make a trigger
    first thing i wanna do is make an area that will detect if the player
    has touched or is inside of it and print that it is working

--- Scenes/dkuskrbfuckingaround/stage1.gd
+++ Scenes/dkuskrbfuckingaround/stage1.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@onready var cam_trigger = $"screen-trigger"
+
+
+func _on_screentrigger_area_entered(area: Area2D) -> void:
+	print("move the camera")
+	pass # Replace with function body.

47d2652fcd5ceb17a091e5d089f507b34c31a2f6
Kenneth Herndon <kengherndon2@gmail.com>
2025-09-05 19:19:37 -0500

    made the signal work

--- Scenes/dkuskrbfuckingaround/stage1.gd
+++ Scenes/dkuskrbfuckingaround/stage1.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var cam_trigger = $"screen-trigger"
 
 
-func _on_screentrigger_area_entered(area: Area2D) -> void:
-	print("move the camera")
+
+
+func _on_screentrigger_body_entered(body: Node2D) -> void:
+	print("camera advance")
 	pass # Replace with function body.

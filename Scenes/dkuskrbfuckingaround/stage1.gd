b5f4a6fd09c7ab092e4292062ab3182288c8c715
Kenneth Herndon <kengherndon2@gmail.com>
2025-09-29 13:35:17 -0500

    transition zoom tween segment

--- Scenes/dkuskrbfuckingaround/stage1.gd
+++ Scenes/dkuskrbfuckingaround/stage1.gd
@@ -13,21 +13,25 @@ func _on_screentrigger_body_entered(body: Node2D) -> void:
 	print("camera advance")
 	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("camera_debug"):
-		if playercamera.is_current():
-			$Player1/wavescreen1.make_current()
-		else:
-			playercamera.make_current()
-			
 #func _physics_process(delta: float) -> void:
 	#if Input.is_action_just_pressed("camera_debug"):
-		#if playercamera == $Player1/Camera2D:
-			#_changing_camera($wavescreen1)
-			#print("debug: advancing")
+		#if playercamera.is_current():
+			#$Player1/wavescreen1.make_current()
 		#else:
-			#_changing_camera(playercamera)
-			#print("debug: else")
+			#playercamera.make_current()
+			
+func _ready() -> void:
+	$transition_camera.make_current()
+	_changing_camera($Player1/Camera2D)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("camera_debug"):
+		if playercamera == $Player1/Camera2D:
+			_changing_camera($wavescreen1)
+			print("debug: advancing")
+		else:
+			_changing_camera(playercamera)
+			print("debug: else")
 			
 func _changing_camera(desired_camera: Camera2D) -> void: #the (desired-cam) is a stored variable i can loose "camera2" at the top i think
 	if transitionTween:
@@ -35,5 +39,11 @@ func _changing_camera(desired_camera: Camera2D) -> void: #the (desired-cam) is a
 	transitionTween = create_tween()
 	var target_transform: Transform2D = desired_camera.global_transform
 	transitionTween.tween_property(transitioncamera, "global_transform", target_transform, 0.5).set_trans(Tween.TRANS_SINE)
+	
+	if transitionZoomTween:
+		transitionZoomTween.kill()
+	transitionZoomTween = create_tween()
+	var target_zoom: Vector2 = desired_camera.zoom
+	transitionZoomTween.tween_property(transitioncamera, "zoom", target_zoom, 0.5).set_trans(Tween.TRANS_SINE)
 	
 	playercamera = desired_camera

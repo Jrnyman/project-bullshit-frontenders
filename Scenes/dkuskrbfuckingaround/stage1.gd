bd5d6f64d11d31b43c0ccef88d551a7611dfb735
Kenneth Herndon <kengherndon2@gmail.com>
2025-09-29 14:03:38 -0500

    removed other cameras as childs

--- Scenes/dkuskrbfuckingaround/stage1.gd
+++ Scenes/dkuskrbfuckingaround/stage1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var cam_trigger = $"screen-trigger"
 #@onready var camera2 = $wavescreen1
-@onready var playercamera: Camera2D = $Player1/Camera2D #declare what kind of shit a variable is gonna be more often
+@onready var playercamera: Camera2D = $Camera2D #declare what kind of shit a variable is gonna be more often
 @onready var transitioncamera: Camera2D = $"Player1/new-transition_camera"
 
 var transitionTween: Tween #Make all the changes smooth
@@ -22,12 +22,12 @@ func _on_screentrigger_body_entered(body: Node2D) -> void:
 			
 func _ready() -> void:
 	transitioncamera.make_current()
-	_changing_camera($Player1/Camera2D)
+	_changing_camera($Camera2D)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("camera_debug"):
-		if playercamera == $Player1/Camera2D:
-			_changing_camera($Player1/wavescreen1)
+		if playercamera == $Camera2D:
+			_changing_camera($wavescreen1)
 			print("debug: advancing")
 		else:
 			_changing_camera(playercamera)
@@ -48,8 +48,8 @@ func _changing_camera(desired_camera: Camera2D) -> void: #the (desired-cam) is a
 	
 	if transitionOffsetTween:
 		transitionOffsetTween.kill()
-		transitionOffsetTween = create_tween()
-		var target_offset: Vector2 = desired_camera.offset
-		transitionOffsetTween.tween_property(transitioncamera, "offset", target_offset, 0.5).set_trans(Tween.TRANS_SINE)
+	transitionOffsetTween = create_tween()
+	var target_offset: Vector2 = desired_camera.offset
+	transitionOffsetTween.tween_property(transitioncamera, "offset", target_offset, 0.5).set_trans(Tween.TRANS_SINE)
 		
 	playercamera = desired_camera
